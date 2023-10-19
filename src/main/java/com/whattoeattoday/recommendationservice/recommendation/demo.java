84df9ebcca9bd77d78e0cb6538c9c76f8dc09292
Lijie Huang <huanglj666@outlook.com>
2023-10-19 01:24:25 -0400

    update algorithm

--- src/main/java/com/whattoeattoday/recommendationservice/recommendation/demo.java
+++ src/main/java/com/whattoeattoday/recommendationservice/recommendation/demo.java
@@ -1,6 +1,6 @@
 package com.whattoeattoday.recommendationservice.recommendation;
-import java.util.Arrays;
-import java.util.List;
+import java.io.Serializable;
+import java.util.*;
 
 import org.apache.spark.ml.feature.HashingTF;
 import org.apache.spark.ml.feature.IDF;
@@ -21,9 +21,10 @@ import org.apache.spark.sql.types.StructType;
  * @author Lijie Huang lh3158@columbia.edu
  * @date 10/18/23
  */
-public class demo {
+public class demo implements Serializable {
+    private static transient SparkSession spark;
     public static void tfidf() {
-        SparkSession spark = SparkSession
+        spark = SparkSession
                 .builder()
                 .appName("Java Spark SQL basic example")
                 .config("spark.master", "local")
@@ -49,19 +50,21 @@ public class demo {
                 .option("user", username)
                 .option("password", password)
                 .load();
-        Dataset<Row> foodData = mysqlData.select("id", "title");
+        StructType schema = new StructType(new StructField[]{
+                new StructField("id", DataTypes.IntegerType, false, Metadata.empty()),
+                new StructField("sentence", DataTypes.StringType, false, Metadata.empty())
+        });
+        Dataset<Row> food = mysqlData.select("id", "title").where("title is not null");
+        Dataset<Row> foodData = spark.createDataFrame(food.toDF().toJavaRDD(), schema);
+        foodData.show();
 
-
-//        StructType schema = new StructType(new StructField[]{
-//                new StructField("id", DataTypes.IntegerType, false, Metadata.empty()),
-//                new StructField("sentence", DataTypes.StringType, false, Metadata.empty())
-//        });
 //        Dataset<Row> sentenceData = spark.createDataFrame(data, schema);
 
         Tokenizer tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words");
         Dataset<Row> wordsData = tokenizer.transform(foodData);
+        wordsData.show();
 
-        int numFeatures = 20;
+        int numFeatures = 100;
         HashingTF hashingTF = new HashingTF()
                 .setInputCol("words")
                 .setOutputCol("rawFeatures")
@@ -76,18 +79,45 @@ public class demo {
         Dataset<Row> rescaledData = idfModel.transform(featurizedData);
         rescaledData.select("id", "features").show();
         Dataset<Row> select = rescaledData.select("id", "features");
-        select.createOrReplaceTempView("people");
-        Dataset<Row> sqlDF = spark.sql("SELECT * FROM people WHERE id=0");
+        select.createOrReplaceTempView("view");
+        Dataset<Row> sqlDF = spark.sql("SELECT * FROM view WHERE id=0");
         List<Row> rows = sqlDF.collectAsList();
         Vector vector = (Vector) rows.get(0).get(1);
+        System.out.println("vector id=0:"+vector);
+        PriorityQueue<Row> pq = new PriorityQueue<>(new Comparator<Row>() {
+            @Override
+            public int compare(Row o1, Row o2) {
+                return (int)((double) o1.get(1) - (double) o2.get(1));
+            }
+        });
+        List<Row> resultList = new ArrayList<>();
 
-        select.foreach(row -> {
+        List<Row> rowsList = select.collectAsList();
+        for (Row row : rowsList) {
             System.out.println(row.get(0));
             Vector curVector = (Vector) row.get(1);
             double sqdist = Vectors.sqdist(vector, curVector);
             System.out.println(sqdist);
-        });
-
+            Row resultRow = RowFactory.create(row.get(0), sqdist);
+            pq.add(resultRow);
+        }
+//        select.foreach(row -> {
+//            System.out.println(row.get(0));
+//            Vector curVector = (Vector) row.get(1);
+//            double sqdist = Vectors.sqdist(vector, curVector);
+//            System.out.println(sqdist);
+//            Row resultRow = RowFactory.create(row.get(0), sqdist);
+//            pq.add(resultRow);
+//            if (pq.size() > 10) {
+//                pq.poll();
+//            }
+//        });
+        while (!pq.isEmpty()) {
+            resultList.add(pq.poll());
+        }
+        System.out.println(resultList);
+        resultList.forEach(System.out::println);
+        spark.close();
     }
 
     public static void main(String[] args) {
