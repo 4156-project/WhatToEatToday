13f26df17abde408423f75fa836215de9bb5bd1b
Lijie Huang <huanglj666@outlook.com>
2023-11-21 11:56:04 -0500

    add querycontent api

---
+++ src/main/java/com/whattoeattoday/recommendationservice/database/request/row/QueryContentInTableRequest.java
@@ -0,0 +1,6 @@
+package com.whattoeattoday.recommendationservice.database.request.row;/**
+ * @author Lijie Huang lh3158@columbia.edu
+ * @date 11/21/23
+ */
+public class QueryContentInTableRequest {
+}
---
+++ src/main/java/com/whattoeattoday/recommendationservice/query/request/QueryContentRequest.java
@@ -0,0 +1,6 @@
+package com.whattoeattoday.recommendationservice.query.request;/**
+ * @author Lijie Huang lh3158@columbia.edu
+ * @date 11/21/23
+ */
+public class QueryContentRequest {
+}
