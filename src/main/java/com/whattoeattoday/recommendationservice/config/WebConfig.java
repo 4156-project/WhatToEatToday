68f9f61017517eaab22a8954ac2b202e9df851be
rxxnnnnn <ruoxuanwang1999@gmail.com>
2023-11-29 20:02:06 -0500

    Add WebConfig

---
+++ src/main/java/com/whattoeattoday/recommendationservice/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.whattoeattoday.recommendationservice.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+/**
+ * @author Ruoxuan Wang
+ * @Date Nov. 29
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // or your client's origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
